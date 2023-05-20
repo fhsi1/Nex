e3fcfdfd0ebfbe06721bc0a53c607eca181b0de5
Yujean Cho <eugenie8@kakao.com>
2023-05-20 22:42:42 +0900

    [UPDATE] binding eventVC

--- Glitch/Controllers/Event/EventVC.swift
+++ Glitch/Controllers/Event/EventVC.swift
@@ -73,16 +73,49 @@ class EventVC: UIViewController {
 
 extension EventVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 8
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventTableViewCell", for: indexPath) as! EventTableViewCell
         
+        switch indexPath.row {
+        case 0:
+            break
+        case 1:
+            cell.elementView.titleLabel.text = "KewlPass NFT"
+            cell.elementView.logoView.image = UIImage(named: "KewlPass")
+            cell.elementView.viewCntLabel.text = "View 1,391"
+        case 2:
+            cell.elementView.titleLabel.text = "Borrowers Rush Week 2"
+            cell.elementView.logoView.image = UIImage(named: "BorrowersRush")
+            cell.elementView.viewCntLabel.text = "View 2,331"
+        case 3:
+            cell.elementView.titleLabel.text = "iZUMi x Syncera Partnership ..."
+            cell.elementView.logoView.image = UIImage(named: "iZUMi")
+            cell.elementView.viewCntLabel.text = "View 1,300"
+        case 4:
+            cell.elementView.titleLabel.text = "bit’s Tribute to Classical NFTs ..."
+            cell.elementView.logoView.image = UIImage(named: "bits")
+            cell.elementView.viewCntLabel.text = "View 300"
+        case 5:
+            cell.elementView.titleLabel.text = "CrusSwap Community Mission ..."
+            cell.elementView.logoView.image = UIImage(named: "CrusSwap")
+            cell.elementView.viewCntLabel.text = "View 228"
+        case 6:
+            cell.elementView.titleLabel.text = "League of Traders May Giveaw ..."
+            cell.elementView.logoView.image = UIImage(named: "League")
+            cell.elementView.viewCntLabel.text = "View 5,212"
+        case 7:
+            cell.elementView.titleLabel.text = "TaskFi AI Protocol TaskFi AIWe..."
+            cell.elementView.logoView.image = UIImage(named: "TaskFi")
+            cell.elementView.viewCntLabel.text = "View 72"
+        default:
+            break
+        }
+        
         cell.selectionStyle = .none
         
         return cell
     }
-    
-    
 }
